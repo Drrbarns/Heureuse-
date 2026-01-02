927fb573d635516b9d08e919bae51c39428a1898
Mortuary System <you@example.com>
2026-01-02 09:31:19 +0000

    Use object-contain for Industries image to show full image without cropping

--- components/sections/IndustriesGrid.tsx
+++ components/sections/IndustriesGrid.tsx
@@ -84,13 +84,13 @@ export default function IndustriesGrid() {
                     </ScrollAnimation>
                 </motion.div>
 
-                <div className="relative h-[400px] lg:h-[600px] w-full rounded-2xl overflow-hidden shadow-2xl bg-white/5 border border-white/10 group mt-8 lg:mt-0">
-                    <motion.div style={{ scale: imageScale, y: imageY }} className="absolute inset-0 w-full h-[110%] -top-[5%] will-change-transform"> {/* Reduced height for parallax buffer */}
+                <div className="relative h-[400px] lg:h-[600px] w-full rounded-2xl overflow-hidden shadow-2xl bg-black/20 border border-white/10 group mt-8 lg:mt-0">
+                    <motion.div style={{ scale: imageScale, y: imageY }} className="absolute inset-0 w-full h-[110%] -top-[5%] will-change-transform flex items-center justify-center"> {/* Reduced height for parallax buffer */}
                         <Image
                             src="/tankk.jpg"
                             alt="Industry Operations"
                             fill
-                            className="object-cover"
+                            className="object-contain"
                             sizes="(max-width: 1024px) 100vw, 50vw"
                         />
                     </motion.div>
